c7c0d95836c78071d757d54a7396a2f26325a306
WahabDev12 <nerdyshinobi12@gmail.com>
2022-06-13 10:56:14 +0000

    ready for deploy

--- backend/server.js
+++ backend/server.js
@@ -26,21 +26,19 @@ app.use('/api/auth', userRoutes)
 app.use('/api/link', linkRoutes)
 
 
-// if (process.env.NODE_ENV === 'production') {
-//   app.use(express.static(path.join(__dirname, '/frontend/build')))
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, '/frontend/build')))
 
-//   app.get('*', (req, res) =>
-//     res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'))
-//   )
-// } else {
-//   app.get('/', (req, res) => {
-//     res.send('API is running....')
-//   })
-// }
+  app.get('*', (req, res) =>
+    res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'))
+  )
+} else {
+  app.get('/', (req, res) => {
+    res.send('API is running....')
+  })
+}
 
-app.get('/', (req, res) => {
-  res.send('API is running....')
-})
+
 
 app.use(notFound)
 app.use(errorHandler)
